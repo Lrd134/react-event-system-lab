17dcf7188f20abd909a56e42af53a38d5c25cfad
Lrd134 <larryc3200@gmail.com>
2021-11-04 13:54:58 -0400

    commit tree 9a5d273c1aeed1e4708ec9e5783b590e45c5bd87

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
+import React, {Component} from 'react';
+
 // Code EyesOnMe Component Here
+
+export default class EyesOnMe extends Component {
+
+  handleFocus = () => {
+    console.log('Good!');
+  }
+  handleBlur = () => {
+    console.log('Hey! Eyes on me!');
+  }
+  render(){
+    return (
+      <button className="eyesOnMe"
+        onFocus={this.handleFocus}
+        onBlur={this.handleBlur}>Eyes On Me!</button>
+    )
+  }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
+import React, {Component} from 'react';
 // Code Keypad Component Here
+
+export default class Keypad extends Component {
+
+  handleKeyUp = (eventInfo) => {
+    console.log("Entering password...");
+  }
+  render(){
+    return (
+      <input className="keypad" type="password" onKeyUp={this.handleKeyUp}/>
+    )
+  }
+}
